54c89adb3d5139dc40ff2fa2d847b786a3629cb9
mmutadic@caltech.edu <mmutadic@caltech.edu>
2023-10-25 04:59:25 +0000

    Task 1

--- src/compile.c
+++ src/compile.c
@@ -4,43 +4,42 @@
 #include <stdlib.h>
 
 bool compile_ast(node_t *node) {
-    switch(node->type){
-        case(NUM):{
+    switch (node->type) {
+        case (NUM): {
             printf("movq $%ld, %%rdi\n", ((num_node_t *) node)->value);
             return true;
         }
-        case(PRINT):{
-            if (!compile_ast(((print_node_t *) node)->expr)){
+        case (PRINT): {
+            if (!compile_ast(((print_node_t *) node)->expr)) {
                 return false;
             }
             printf("callq print_int\n");
             return true;
         }
-        case(SEQUENCE):{
+        case (SEQUENCE): {
             sequence_node_t *sequence = (sequence_node_t *) node;
             for (size_t i = 0; i < sequence->statement_count; i++) {
-                if (!compile_ast(sequence->statements[i])){
+                if (!compile_ast(sequence->statements[i])) {
                     return false;
                 }
             }
             return true;
         }
-        case(BINARY_OP):{
+        case (BINARY_OP): {
             break;
         }
-        case(VAR):{
+        case (VAR): {
             break;
         }
-        case(LET):{
+        case (LET): {
             break;
         }
-        case(IF):{
+        case (IF): {
             break;
         }
-        case(WHILE):{
+        case (WHILE): {
             break;
         }
-
     }
     return false; // for now, every statement causes a compilation failure
 }
