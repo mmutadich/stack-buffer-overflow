5d77ffc71097e444ac3a678cec6359cfb9142831
mmutadic@caltech.edu <mmutadic@caltech.edu>
2023-10-25 04:59:19 +0000

    task 1

--- src/compile.c
+++ src/compile.c
@@ -4,8 +4,43 @@
 #include <stdlib.h>
 
 bool compile_ast(node_t *node) {
-    /* You should remove this cast to void in your solution.
-     * It is just here so the code compiles without warnings. */
-    (void) node;
+    switch(node->type){
+        case(NUM):{
+            printf("movq $%ld, %%rdi\n", ((num_node_t *) node)->value);
+            return true;
+        }
+        case(PRINT):{
+            if (!compile_ast(((print_node_t *) node)->expr)){
+                return false;
+            }
+            printf("callq print_int\n");
+            return true;
+        }
+        case(SEQUENCE):{
+            sequence_node_t *sequence = (sequence_node_t *) node;
+            for (size_t i = 0; i < sequence->statement_count; i++) {
+                if (!compile_ast(sequence->statements[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+        case(BINARY_OP):{
+            break;
+        }
+        case(VAR):{
+            break;
+        }
+        case(LET):{
+            break;
+        }
+        case(IF):{
+            break;
+        }
+        case(WHILE):{
+            break;
+        }
+
+    }
     return false; // for now, every statement causes a compilation failure
 }
